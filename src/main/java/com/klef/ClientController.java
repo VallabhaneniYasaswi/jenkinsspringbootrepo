c3bb1df5d028a08b6e659004237f9f2c92ceebb3
Yasaswi <yasaswivallabhaneni11@gmail.com>
2025-08-09 17:01:35 +0530

    updated project

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -14,5 +14,9 @@ public class ClientController {
 	public String demo() {
 		return "Jenkins Demo";
 	}
+	@GetMapping("/test")
+	public String test() {
+		return "Deploy Spring Boot using Jenkins";
+	}
 
 }
